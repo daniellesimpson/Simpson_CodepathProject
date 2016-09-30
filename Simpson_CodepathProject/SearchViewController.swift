c5c86b140fc1dcb243af8e02b10ba161c24733e2
Danielle Simpson <danielle.simpson2@capitalone.com>
2016-09-29 22:32:31 -0400

    struct built. code and api working. CollectionView is not working.

--- Simpson_CodepathProject/SearchViewController.swift
+++ Simpson_CodepathProject/SearchViewController.swift
@@ -7,41 +7,29 @@
 //
 
 import UIKit
+import Foundation
 
 
-internal struct movieSearchResult {
-    
-    //Movie information
-     let image: UIImage
-    
-     let movieTitle: String
-    
-     let releaseDate: String
-    
-     let movieSynopsis: String
-    
-    //User information
-    internal let username: String
-    
-    internal let checkTime: NSDate
-    
-    internal let profilePic: UIImage
-}
-
-class SearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    
+class SearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UISearchBarDelegate {
     
     @IBOutlet weak var searchCollectionView: UICollectionView!
     
-    var myMovies: [NSDictionary]! = []
-    var results: [[String : AnyObject]] = []
-    var originalTitle = "original_title"
-    var posterPath = "poster_path"
-    var moviePosterPath: String!
-    var imageUrl: NSURL!
-    var imageData: NSData!
+    //var myMovies: [NSDictionary]! = []
+    //var tempResults: [[String : AnyObject]] = []
     
+    //var originalTitle = "title"
+    //var posterPath = "poster_path"
+    //var moviePosterPath: String!
+    //var imageUrl: NSURL!
+    //var imageData: NSData!
     
+    @IBOutlet weak var mySearchTextfield: UITextField!
+    var searchText: String!
+    var myApi = "https://api.themoviedb.org/3/search/movie?api_key=ced1b9e67f9f9c305424201cdfaa3532&language=en-US&query="
+    var searchMovieApi: String!
+    
+    var tempResults = [Movies]()
+
     
 
     override func viewDidLoad() {
@@ -59,62 +47,81 @@ class SearchViewController: UIViewController, UICollectionViewDataSource, UIColl
         searchCollectionView.delegate = self
         searchCollectionView.backgroundColor = UIColorFromRGB(0xE2E3E4)
         SearchCollectionViewCell.appearance().backgroundColor = UIColorFromRGB(0xffffff)
-        
-        //running the API
-        let url = NSURL(string: "https://api.themoviedb.org/3/movie/popular?api_key=ced1b9e67f9f9c305424201cdfaa3532&language=en-US")
-        let request = NSURLRequest(URL: url!)
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response:NSURLResponse?, data:NSData?, error:NSError?) in
-            
-            let myDictionary = try! NSJSONSerialization.JSONObjectWithData(data!, options: [])
-            let dict = myDictionary as! [String : AnyObject]
-            self.results = dict["results"] as! [[String : AnyObject]]
-            
 
-            
-            
-            self.searchCollectionView.reloadData()
-            
-            //print(myDictionary)
-            
-            
-        }
         
-
     }
     
     
+    
+    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return results.count
+        return tempResults.count
+        
     }
     
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
         let cell = searchCollectionView.dequeueReusableCellWithReuseIdentifier("searchCell", forIndexPath: indexPath) as! SearchCollectionViewCell
         
-        //let movieNames = results[indexPath.row][originalTitle] as! String
+        let newResult = tempResults[indexPath.row]
         
-        //print(movieNames)
-        
-        cell.movieTitleLabel.text = results[indexPath.row][originalTitle] as! String
-        
-        self.moviePosterPath = results[indexPath.row][posterPath] as! String
-        self.imageUrl = NSURL(string: "https://image.tmdb.org/t/p/w500\(self.moviePosterPath)")
-        self.imageData = NSData(contentsOfURL: self.imageUrl)
-        
-        cell.searchMoviePoster.image = UIImage(data: imageData)
+        cell.movieTitleLabel.text = newResult.movieTitle
+        cell.searchMoviePoster.image = UIImage(data: newResult.imageData)
         
         return cell
     }
     
+
+    func makeCall() {
+        _ = ["content-type": "application/json"]
+        let parameters = []
+        
+        var postData = NSData()
+        
+        do {
+            postData = try NSJSONSerialization.dataWithJSONObject(parameters, options: .PrettyPrinted)
+            
+        }
+        catch _ {
+            print("FAIL")
+        }
+        
+        let url = NSURL(string: searchMovieApi!)
+        
+        NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, response, error) in
+            
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            let myDictionary = try! NSJSONSerialization.JSONObjectWithData(data!, options: [])
+            let dict = myDictionary as! [String : AnyObject]
+            let holder = dict["results"] as! [[String : AnyObject]]
+            
+            for index in holder {
+                let movie = Movies(result: index)
+                self.tempResults.append(movie)
+                self.searchCollectionView.reloadData()
+            }
+            
+        }.resume()
+    }
     
+
+
     
-    
-    
-    
-    
-    
-    
-    
+    @IBAction func searchNow(sender: AnyObject) {
+        
+        let textInput = mySearchTextfield.text! as String
+        searchText = textInput.stringByReplacingOccurrencesOfString(" ", withString: "%20")
+        searchMovieApi = (myApi + searchText)
+        makeCall()
+        
+
+    }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ Simpson_CodepathProject/movieStruct.swift
@@ -0,0 +1,43 @@
+//
+//  movieStruct.swift
+//  Simpson_CodepathProject
+//
+//  Created by Simpson, Danielle on 9/29/16.
+//  Copyright © 2016 Simpson, Danielle. All rights reserved.
+//
+
+import Foundation
+
+struct Movies {
+    var movieTitle: String!
+    var movieImage: NSData!
+    var imagePath: String!
+    var imageURL: NSURL!
+    var imageData: NSData!
+    var movieDescription: String!
+
+    init (result: [String : AnyObject]) {
+        self.movieTitle = result["original_title"] as! String
+        self.imagePath = result["poster_path"] as! String
+        self.imageURL = NSURL(string: "https://image.tmdb.org/t/p/w500\(self.imagePath)")
+        self.imageData = NSData(contentsOfURL: self.imageURL)
+        
+        print(movieTitle)
+        print(imagePath)
+        print(imageURL)
+    }
+
+
+    
+
+    init(movieTitle: String, movieImage: NSData, movieDescription: String, imagePath: String) {
+
+        self.movieTitle = movieTitle
+        self.movieImage = nil
+        self.imagePath = imagePath
+        self.movieDescription = movieDescription
+
+}
+
+
+}
